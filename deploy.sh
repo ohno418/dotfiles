753e4e808a07f0ec6b2ba2fab2e1d28fa8b9e1cd
ohno418 <yutaro.ono.418@gmail.com>
2022-01-04 23:48:32 +0900

    Remove i3

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@ set -e
 
 ln -sf $(pwd)/gitconfig ~/.gitconfig
 ln -sf $(pwd)/vimrc ~/.vimrc
-ln -sf $(pwd)/i3_config ~/.config/i3/config
-# ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $(pwd)/tmux.conf ~/.tmux.conf
 
 echo OK
