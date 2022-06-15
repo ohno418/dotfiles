86897f28bfda260570d5b6f0a6c20ac14e2ab8fe
ohno418 <yutaro.ono.418@gmail.com>
2022-06-15 15:01:29 +0900

    chore

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ set -e
 ln -sf $(pwd)/gitconfig ~/.gitconfig
 ln -sf $(pwd)/nvim.vim ~/.config/nvim/init.vim
 ln -sf $(pwd)/tmux.conf ~/.tmux.conf
-ln -sf $(pwd)/alacritty.yml ~/.alacritty.yml
+ln -sf $(pwd)/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 echo OK
