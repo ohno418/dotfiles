55fb42b7b40c8cd920e0e44b6e6caa5b476cfba1
ohno418 <yutaro.ono.418@gmail.com>
2022-08-02 14:25:54 +0900

    Move tmux config file to ~/.config/tmux/tmux.conf

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -e
 
 ln -sf $(pwd)/gitconfig ~/.gitconfig
 ln -sf $(pwd)/nvim.vim ~/.config/nvim/init.vim
-ln -sf $(pwd)/tmux.conf ~/.tmux.conf
+ln -sf $(pwd)/tmux.conf ~/.config/tmux/tmux.conf
 ln -sf $(pwd)/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 # ln -sf $(pwd)/i3/config ~/.config/i3/config
