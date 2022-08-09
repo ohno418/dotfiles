d759a828ae75f7dc4349921697ebab62b9d248f8
ohno418 <yutaro.ono.418@gmail.com>
2022-08-09 19:31:05 +0900

    Use init.vim over init.lua

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@ set -e
 
 mkdir -p ~/.config/nvim ~/.config/tmux ~/.config/alacritty
 ln -sf $(pwd)/gitconfig ~/.gitconfig
-ln -sf $(pwd)/nvim/init.lua ~/.config/nvim/init.lua
+ln -sf $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
+# ln -sf $(pwd)/nvim/init.lua ~/.config/nvim/init.lua
 ln -sf $(pwd)/tmux.conf ~/.config/tmux/tmux.conf
 ln -sf $(pwd)/alacritty.yml ~/.config/alacritty/alacritty.yml
 
