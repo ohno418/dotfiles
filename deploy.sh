58a2cc45dbc62170d619293eb85b9951b487bfc1
ohno418 <yutaro.ono.418@gmail.com>
2022-08-29 23:32:29 +0900

    foot: Put template config for foot

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,9 @@ ln -sf $(pwd)/sway/config ~/.config/sway/config
 ln -sf $(pwd)/sway/bar.sh ~/.config/sway/bar.sh
 ln -sf $(pwd)/sway/rofi-config.rasi ~/.config/rofi/config.rasi
 
-# Alacritty
-# mkdir -p ~/.config/alacritty
-# ln -sf $(pwd)/alacritty.yml ~/.config/alacritty/alacritty.yml
+# foot
+mkdir -p ~/.config/foot
+ln -sf $(pwd)/foot.ini ~/.config/foot/foot.ini
 
 # i3
 # mkdir -p ~/.config/i3
@@ -22,5 +22,9 @@ ln -sf $(pwd)/sway/rofi-config.rasi ~/.config/rofi/config.rasi
 # ln -sf $(pwd)/i3/config ~/.config/i3/config
 # ln -sf $(pwd)/i3/bar.sh ~/.config/i3/bar.sh
 # ln -sf $(pwd)/i3/rofi-config.rasi ~/.config/rofi/config.rasi
+
+# Alacritty
+# mkdir -p ~/.config/alacritty
+# ln -sf $(pwd)/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 echo OK
