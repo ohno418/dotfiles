f52173cab663c68d703b2374738d2c2772bb0354
ohno418 <yutaro.ono.418@gmail.com>
2022-08-29 23:26:52 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 set -e
 
-mkdir -p ~/.config/nvim ~/.config/tmux ~/.config/alacritty
+mkdir -p ~/.config/nvim ~/.config/tmux
 ln -sf $(pwd)/gitconfig ~/.gitconfig
 ln -sf $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf $(pwd)/tmux.conf ~/.config/tmux/tmux.conf
-ln -sf $(pwd)/alacritty.yml ~/.config/alacritty/alacritty.yml
-
-# i3
-mkdir -p ~/.config/i3
-mkdir -p ~/.config/rofi
-ln -sf $(pwd)/i3/config ~/.config/i3/config
-ln -sf $(pwd)/i3/bar.sh ~/.config/i3/bar.sh
-ln -sf $(pwd)/i3/rofi-config.rasi ~/.config/rofi/config.rasi
 
 # Sway
-# mkdir -p ~/.config/sway
-# ln -sf $(pwd)/sway/config ~/.config/sway/config
-# ln -sf $(pwd)/sway/bar.sh ~/.config/sway/bar.sh
+mkdir -p ~/.config/sway ~/.config/rofi
+ln -sf $(pwd)/sway/config ~/.config/sway/config
+ln -sf $(pwd)/sway/bar.sh ~/.config/sway/bar.sh
+ln -sf $(pwd)/sway/rofi-config.rasi ~/.config/rofi/config.rasi
+
+# Alacritty
+# mkdir -p ~/.config/alacritty
+# ln -sf $(pwd)/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+# i3
+# mkdir -p ~/.config/i3
+# mkdir -p ~/.config/rofi
+# ln -sf $(pwd)/i3/config ~/.config/i3/config
+# ln -sf $(pwd)/i3/bar.sh ~/.config/i3/bar.sh
+# ln -sf $(pwd)/i3/rofi-config.rasi ~/.config/rofi/config.rasi
 
 echo OK
