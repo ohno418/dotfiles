0735e0942094812040b5992581a336befe98a8d3
Yutaro Ohno <yutaro.ono.418@gmail.com>
2023-01-13 22:39:52 +0900

    dwl: wip

--- dwl_config.h
+++ dwl_config.h
@@ -40,10 +40,7 @@ static const MonitorRule monrules[] = {
 /* keyboard */
 static const struct xkb_rule_names xkb_rules = {
     /* can specify fields: rules, model, layout, variant, options */
-    /* example:
     .options = "ctrl:nocaps",
-    */
-    .options = NULL,
 };
 
 static const int repeat_rate = 50;
@@ -117,39 +114,39 @@ static const Key keys[] = {
     // FIXME: private browser
     // { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_p,          spawn,          {.v = browserprivatecmd} },
 
-    { MODKEY,                    XKB_KEY_j,          focusstack,     {.i = +1} },
-    { MODKEY,                    XKB_KEY_k,          focusstack,     {.i = -1} },
-    { MODKEY,                    XKB_KEY_i,          incnmaster,     {.i = +1} },
-    { MODKEY,                    XKB_KEY_d,          incnmaster,     {.i = -1} },
-    { MODKEY,                    XKB_KEY_h,          setmfact,       {.f = -0.05} },
-    { MODKEY,                    XKB_KEY_l,          setmfact,       {.f = +0.05} },
-    { MODKEY,                    XKB_KEY_Return,     zoom,           {0} },
-    { MODKEY,                    XKB_KEY_Tab,        view,           {0} },
-    { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_C,          killclient,     {0} },
+    { MODKEY,                    XKB_KEY_j,          focusstack,       {.i = +1} },
+    { MODKEY,                    XKB_KEY_k,          focusstack,       {.i = -1} },
+    { MODKEY,                    XKB_KEY_i,          incnmaster,       {.i = +1} },
+    { MODKEY,                    XKB_KEY_d,          incnmaster,       {.i = -1} },
+    { MODKEY,                    XKB_KEY_h,          setmfact,         {.f = -0.05} },
+    { MODKEY,                    XKB_KEY_l,          setmfact,         {.f = +0.05} },
+    { MODKEY,                    XKB_KEY_Return,     zoom,             {0} },
+    { MODKEY,                    XKB_KEY_Tab,        view,             {0} },
+    { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_C,          killclient,       {0} },
 
-    { MODKEY,                    XKB_KEY_t,          setlayout,      {.v = &layouts[0]} },
-    // { MODKEY,                    XKB_KEY_f,          setlayout,      {.v = &layouts[1]} },
-    { MODKEY,                    XKB_KEY_m,          setlayout,      {.v = &layouts[2]} },
-    { MODKEY,                    XKB_KEY_space,      setlayout,      {0} },
+    { MODKEY,                    XKB_KEY_t,          setlayout,        {.v = &layouts[0]} },
+    // { MODKEY,                    XKB_KEY_f,          setlayout,        {.v = &layouts[1]} },
+    { MODKEY,                    XKB_KEY_m,          setlayout,        {.v = &layouts[2]} },
+    { MODKEY,                    XKB_KEY_space,      setlayout,        {0} },
 
-    { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_space,      togglefloating, {0} },
-    { MODKEY,                    XKB_KEY_f,         togglefullscreen, {0} },
-    { MODKEY,                    XKB_KEY_0,          view,           {.ui = ~0} },
-    { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_parenright, tag,            {.ui = ~0} },
-    { MODKEY,                    XKB_KEY_comma,      focusmon,       {.i = WLR_DIRECTION_LEFT} },
-    { MODKEY,                    XKB_KEY_period,     focusmon,       {.i = WLR_DIRECTION_RIGHT} },
-    { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_less,       tagmon,         {.i = WLR_DIRECTION_LEFT} },
-    { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_greater,    tagmon,         {.i = WLR_DIRECTION_RIGHT} },
-    TAGKEYS(          XKB_KEY_1, XKB_KEY_exclam,                     0),
-    TAGKEYS(          XKB_KEY_2, XKB_KEY_at,                         1),
-    TAGKEYS(          XKB_KEY_3, XKB_KEY_numbersign,                 2),
-    TAGKEYS(          XKB_KEY_4, XKB_KEY_dollar,                     3),
-    TAGKEYS(          XKB_KEY_5, XKB_KEY_percent,                    4),
-    TAGKEYS(          XKB_KEY_6, XKB_KEY_asciicircum,                5),
-    TAGKEYS(          XKB_KEY_7, XKB_KEY_ampersand,                  6),
-    TAGKEYS(          XKB_KEY_8, XKB_KEY_asterisk,                   7),
-    TAGKEYS(          XKB_KEY_9, XKB_KEY_parenleft,                  8),
-    { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Q,          quit,           {0} },
+    { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_space,      togglefloating,   {0} },
+    { MODKEY,                    XKB_KEY_f,          togglefullscreen, {0} },
+    { MODKEY,                    XKB_KEY_0,          view,             {.ui = ~0} },
+    { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_parenright, tag,              {.ui = ~0} },
+    { MODKEY,                    XKB_KEY_comma,      focusmon,         {.i = WLR_DIRECTION_LEFT} },
+    { MODKEY,                    XKB_KEY_period,     focusmon,         {.i = WLR_DIRECTION_RIGHT} },
+    { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_less,       tagmon,           {.i = WLR_DIRECTION_LEFT} },
+    { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_greater,    tagmon,           {.i = WLR_DIRECTION_RIGHT} },
+    TAGKEYS(          XKB_KEY_1, XKB_KEY_exclam,                       0),
+    TAGKEYS(          XKB_KEY_2, XKB_KEY_at,                           1),
+    TAGKEYS(          XKB_KEY_3, XKB_KEY_numbersign,                   2),
+    TAGKEYS(          XKB_KEY_4, XKB_KEY_dollar,                       3),
+    TAGKEYS(          XKB_KEY_5, XKB_KEY_percent,                      4),
+    TAGKEYS(          XKB_KEY_6, XKB_KEY_asciicircum,                  5),
+    TAGKEYS(          XKB_KEY_7, XKB_KEY_ampersand,                    6),
+    TAGKEYS(          XKB_KEY_8, XKB_KEY_asterisk,                     7),
+    TAGKEYS(          XKB_KEY_9, XKB_KEY_parenleft,                    8),
+    { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Q,          quit,             {0} },
 
     /* Ctrl-Alt-Backspace and Ctrl-Alt-Fx used to be handled by X server */
     { WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT,XKB_KEY_Terminate_Server, quit, {0} },
