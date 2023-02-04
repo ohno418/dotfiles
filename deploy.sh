181e4db68b05ab9296070fd754fd327fea73d656
Yutaro Ohno <yutaro.ono.418@gmail.com>
2023-02-04 22:28:20 +0900

    deploy.sh: remove river settings

--- deploy.sh
+++ deploy.sh
@@ -14,12 +14,6 @@ ln -sf $(pwd)/tmux.conf ~/.config/tmux/tmux.conf
 mkdir -p ~/.config/foot
 ln -sf $(pwd)/foot.ini ~/.config/foot/foot.ini
 
-# river
-mkdir -p ~/.config/river ~/.config/yambar
-ln -sf $(pwd)/river/river_init ~/.config/river/init
-ln -sf $(pwd)/river/wallpaper.jpg ~/.config/river/wallpaper.jpg
-ln -sf $(pwd)/river/yambar_config.yml ~/.config/yambar/config.yml
-
 # Sway
 # mkdir -p ~/.config/sway
 # ln -sf $(pwd)/sway/config ~/.config/sway/config
