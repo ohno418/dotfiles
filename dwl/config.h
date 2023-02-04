b54f02ab179c65b6db952ab62278888916ebae5a
Yutaro Ohno <yutaro.ono.418@gmail.com>
2023-02-04 20:45:12 +0900

    dwl: wip

--- dwl/config.h
+++ dwl/config.h
@@ -27,6 +27,7 @@ static const Layout layouts[] = {
     { "[M]",      monocle },
 };
 
+// TODO: Set up monitors
 /* monitors */
 static const MonitorRule monrules[] = {
     /* name       mfact nmaster scale layout       rotate/reflect */
@@ -166,8 +167,9 @@ static const Key keys[] = {
 
     { MODKEY|WLR_MODIFIER_SHIFT|WLR_MODIFIER_LOGO, XKB_KEY_Q, quit, {0} },
 
+    // TODO: ??
     /* Ctrl-Alt-Backspace and Ctrl-Alt-Fx used to be handled by X server */
-    { WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT,XKB_KEY_Terminate_Server, quit, {0} },
+    { WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT, XKB_KEY_Terminate_Server, quit, {0} },
 #define CHVT(n) { WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT,XKB_KEY_XF86Switch_VT_##n, chvt, {.ui = (n)} }
     CHVT(1), CHVT(2), CHVT(3), CHVT(4), CHVT(5), CHVT(6),
     CHVT(7), CHVT(8), CHVT(9), CHVT(10), CHVT(11), CHVT(12),
