d8ba9bd95ed436e46b447dbcba786e94f1cfef6f
Yutaro Ohno <yutaro.ono.418@gmail.com>
2023-02-15 13:30:55 +0900

    dwl: use mod+shift+q to kill a client

--- dwl/config.h
+++ dwl/config.h
@@ -145,7 +145,7 @@ static const Key keys[] = {
 	{ MODKEY,                    XKB_KEY_Return,     zoom,             {0} },
 	{ MODKEY,                    XKB_KEY_Tab,        view,             {0} },
 	{ MODKEY,                    XKB_KEY_o,          view,             {0} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_C,          killclient,       {0} },
+	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Q,          killclient,       {0} },
 	{ MODKEY,                    XKB_KEY_t,          setlayout,        {.v = &layouts[0]} },
 	{ MODKEY,                    XKB_KEY_f,          setlayout,        {.v = &layouts[1]} },
 	{ MODKEY,                    XKB_KEY_m,          setlayout,        {.v = &layouts[2]} },
