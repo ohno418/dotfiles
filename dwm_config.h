87a14b5cc997d80ae05cfa383bf40755b6be91c0
Yutaro Ohno <yutaro.ono.418@gmail.com>
2023-02-28 21:20:59 +0900

    dwm: open Firefox with tag 9

--- dwm_config.h
+++ dwm_config.h
@@ -28,7 +28,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
