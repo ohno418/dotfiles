2c5d195d93d605acd60e0a789be91572d58fe381
Yutaro Ohno <yutaro.ono.418@gmail.com>
2023-03-07 15:39:35 +0900

    deploy.sh: put the git config file in ~/.config/git

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ ln -sf $(pwd)/zsh/zshenv ~/.zshenv
 ln -sf $(pwd)/zsh/zprofile ~/.zprofile
 ln -sf $(pwd)/zsh/zshrc ~/.zshrc
 
-mkdir -p ~/.config/nvim ~/.config/tmux
-ln -sf $(pwd)/gitconfig ~/.gitconfig
+mkdir -p ~/.config/git ~/.config/nvim ~/.config/tmux
+ln -sf $(pwd)/gitconfig ~/.config/git/config
 ln -sf $(pwd)/nvim_init.lua ~/.config/nvim/init.lua
 ln -sf $(pwd)/tmux.conf ~/.config/tmux/tmux.conf
 
