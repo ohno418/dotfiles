7f3470af960493f3545ac9bf3e5b1727f29188ae
Yutaro Ohno <yutaro.ono.418@gmail.com>
2023-04-28 00:16:28 +0900

    nvim: tweak netrw settings

--- nvim_init.lua
+++ nvim_init.lua
@@ -53,7 +53,7 @@ vim.g.netrw_liststyle = 3 -- treeview
 vim.g.netrw_browser_split = 4 -- open in a prior window
 vim.g.netrw_altv = 1 -- open splits to the right
 vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
+vim.g.netrw_winsize = 20
 
 -------------
 -- Plugins --
