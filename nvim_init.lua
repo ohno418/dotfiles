0dcf0a52cd7a6ae14d6ea2f1c42fd45221d47bfe
Yutaro Ohno <yutaro.ono.418@gmail.com>
2023-11-24 11:39:50 +0900

    vim: not load netrw, in favor of nvim-tree

--- nvim_init.lua
+++ nvim_init.lua
@@ -1,6 +1,10 @@
 -------------
 -- Options --
 -------------
+-- Disable netrw in favor of nvim-tree.
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 local options = {
   -- basics
   number = true,
