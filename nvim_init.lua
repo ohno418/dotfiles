66dcad9fcd19b32870879f3f996bf6692ea5c1a0
Yutaro Ohno <yutaro.ono.418@gmail.com>
2024-05-06 13:17:01 +0900

    nvim: add lualine for status bar

--- nvim_init.lua
+++ nvim_init.lua
@@ -227,6 +227,37 @@ require('lazy').setup({
       })
     end
   },
+
+  -- Status line
+  {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      require('lualine').setup {
+        options = {
+          icons_enabled = false,
+          theme = 'auto',
+          component_separators = { left = '|', right = '|'},
+          section_separators = { left = '', right = ''},
+        },
+        sections = {
+          lualine_a = { 'filename' },
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = { 'searchcount' },
+          lualine_z = {},
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = { 'filename' },
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {},
+        },
+      }
+    end
+  },
 })
 
 -------------------------
