b08b28202a350d258c755e1e31c64546472686f0
Yutaro Ohno <yutaro.ono.418@gmail.com>
2024-07-22 18:05:09 +0900

    nvim: lueline: display relative path for file

--- nvim_init.lua
+++ nvim_init.lua
@@ -182,7 +182,12 @@ require('lazy').setup({
         sections = {
           lualine_a = { 'mode' },
           lualine_b = { 'searchcount' },
-          lualine_c = { 'filename' },
+          lualine_c = {
+            {
+              'filename',
+              path = 1,
+            },
+          },
           lualine_x = {},
           lualine_y = { 'progress' },
           lualine_z = { 'location' },
@@ -190,7 +195,12 @@ require('lazy').setup({
         inactive_sections = {
           lualine_a = {},
           lualine_b = {},
-          lualine_c = { 'filename' },
+          lualine_c = {
+            {
+              'filename',
+              path = 1,
+            },
+          },
           lualine_x = { 'location' },
           lualine_y = {},
           lualine_z = {},
