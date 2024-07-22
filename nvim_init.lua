de44662bb3f5705c20863e051d997bb5a0a42a5c
Yutaro Ohno <yutaro.ono.418@gmail.com>
2024-07-22 19:13:16 +0900

    nvim: harpoon: wrap on moving next/prev

--- nvim_init.lua
+++ nvim_init.lua
@@ -174,8 +174,8 @@ require('lazy').setup({
       harpoon:setup()
       vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
       vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-      vim.keymap.set('n', '<C-n>', function() harpoon:list():next() end)
-      vim.keymap.set('n', '<C-p>', function() harpoon:list():prev() end)
+      vim.keymap.set('n', '<C-n>', function() harpoon:list():next({ ui_nav_wrap = true }) end)
+      vim.keymap.set('n', '<C-p>', function() harpoon:list():prev({ ui_nav_wrap = true }) end)
     end
   },
 
