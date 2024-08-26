29f99d59dff9bd249362a024c3411b300aab1787
Yutaro Ohno <yutaro.ono.418@gmail.com>
2024-08-26 18:34:29 +0900

    nvim: lazy: make bootstrap script up to date

--- nvim_init.lua
+++ nvim_init.lua
@@ -58,18 +58,22 @@ vim.keymap.set('n', 'gl', '$')
 vim.keymap.set('n', 'gm', '%')
 
 -- PLUGINS --
--- Using lazy.nvim as the package manager.
--- (ref: https://github.com/folke/lazy.nvim#-installation)
+-- Bootstrap lazy.nvim plugin manager.
+-- (ref: https://lazy.folke.io/installation)
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none",
+                              "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
