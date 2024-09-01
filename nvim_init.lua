9f1d3c9d7e3e7b5c0d3bdfd7f2e329ec5e18cdb3
Yutaro Ohno <yutaro.ono.418@gmail.com>
2024-09-01 13:59:23 +0900

    nvim: tweak listchars

--- nvim_init.lua
+++ nvim_init.lua
@@ -16,7 +16,12 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.smartindent = true
 vim.opt.list = true
-vim.opt.listchars = 'tab:> ,lead:·,trail:·,nbsp:+'
+vim.opt.listchars = {
+    tab = '> ',
+    lead = '-',
+    trail = '-',
+    nbsp = '+',
+}
 -- searching
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
