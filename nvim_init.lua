12cbd25510fd8bb3839a9eff1cebb1cc455f3b4a
Yutaro Ohno <yutaro.ono.418@gmail.com>
2024-09-09 00:22:28 +0900

    nvim: lualine: remove unnecessary setting

--- nvim_init.lua
+++ nvim_init.lua
@@ -183,7 +183,6 @@ require('lazy').setup({
       require('lualine').setup {
         options = {
           icons_enabled = false,
-          theme = 'auto',
           component_separators = { left = '', right = '' },
           section_separators = { left = '', right = '' },
           globalstatus = true,
