b4922e08e8e209ef209f4352184cac819b8b1719
Yutaro Ohno <yutaro.ono.418@gmail.com>
2024-09-22 21:26:04 +0900

    nvim: completion: chore

--- nvim_init.lua
+++ nvim_init.lua
@@ -270,9 +270,7 @@ require('lazy').setup({
   -- Autocompletion
   {
     'hrsh7th/nvim-cmp',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-    },
+    dependencies = { 'hrsh7th/cmp-nvim-lsp' },
     config = function()
       local cmp = require('cmp')
       cmp.setup({
