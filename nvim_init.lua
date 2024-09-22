a5981a63a8df0f7a9bbb1cba7ac152d6768f316e
Yutaro Ohno <yutaro.ono.418@gmail.com>
2024-09-22 19:55:43 +0900

    nvim: telescope: use latest version

--- nvim_init.lua
+++ nvim_init.lua
@@ -115,7 +115,6 @@ require('lazy').setup({
   -- Fuzzy finder
   {
     'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys = {
       -- file search
