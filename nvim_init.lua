c7c40b4b5d69a2b52ebc4e11edc41ff59ee59859
Yutaro Ohno <yutaro.ono.418@gmail.com>
2026-05-30 18:21:25 +0900

    nvim: set tabstop to 2 for Lua files

--- nvim_init.lua
+++ nvim_init.lua
@@ -40,6 +40,7 @@ vim.opt.completeopt = { 'fuzzy', 'menu', 'menuone', 'noselect', 'popup' }
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'lua',
   callback = function()
+    vim.opt.tabstop = 2
     vim.opt.shiftwidth = 2
     vim.opt.softtabstop = 2
   end,
